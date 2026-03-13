ac3dc232cb79ec573601bc7d466bd0d49c9589f5
vishabjeetcoolplugins <vishabjeetcoolplugins@gmail.com>
2026-03-13 13:03:14 +0530

    removed extra code

--- assets/bulk-string-translate/index.asset.php
+++ assets/bulk-string-translate/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '9592f8076cfd71a55354');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '767f644a1efe0e233d6a');
