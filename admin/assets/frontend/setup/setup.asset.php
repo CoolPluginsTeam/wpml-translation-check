c22efcab32626890b28db676fa93791948efa35d
vishabjeetcoolplugins <vishabjeetcoolplugins@gmail.com>
2026-03-13 16:47:36 +0530

    build file updated

--- admin/assets/frontend/setup/setup.asset.php
+++ admin/assets/frontend/setup/setup.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => 'de381778733e63501465');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => '8093216f705956f55d88');
