b1317dc9821d3eba2e6af1fcdb20d7fb87e85810
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2026-03-17 16:38:03 +0530

    Textual changes.
    Textual changes.

--- assets/bulk-translate/index.asset.php
+++ assets/bulk-translate/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '299110d18530bfcdca7a');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd0e71eaa48308bd86fb0');
